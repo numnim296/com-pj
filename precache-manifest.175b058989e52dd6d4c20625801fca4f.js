c8bbf3c394f42873214da68526015d4ff5930f62
muthita munmueang <muthita.mu.61@ubu.ac.th>
2020-10-09 21:46:16 +0700

    Updates

---
+++ precache-manifest.175b058989e52dd6d4c20625801fca4f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5682c8c3ae2c53c4d737b857a4b48e28",
+    "url": "/com-pj/index.html"
+  },
+  {
+    "revision": "bc105dc0b2b7b3b93437",
+    "url": "/com-pj/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "db7a627cbc2b935499d5",
+    "url": "/com-pj/static/js/2.a925df73.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/com-pj/static/js/2.a925df73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc105dc0b2b7b3b93437",
+    "url": "/com-pj/static/js/main.0573d21b.chunk.js"
+  },
+  {
+    "revision": "90f9f2846a5a525c2187",
+    "url": "/com-pj/static/js/runtime-main.09424e21.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/com-pj/static/media/logo.5d5d9eef.svg"
+  }
+]);
